c4ce53cd8a1486faa60be9f62166635df4e8d389
Dergacheva Anna <46784887+AnnaDe-v@users.noreply.github.com>
2022-11-08 09:56:40 +0300

    cut URL

--- src/components/Downloader.jsx
+++ src/components/Downloader.jsx
@@ -1,90 +1,95 @@
-import React, { useState } from "react";
-import styles from "./Downloader.module.css";
+import React, { useState } from 'react'
+import styles from './Downloader.module.css'
 
 export default function Downloader() {
-  const [urlValue, setURLValue] = useState("");
-  const [requestStatus, setRequestStatus] = useState("idle");
-  const [isLoading, setIsLoading] = useState(false);
-  const [stateFile, setStateFile] = useState(null);
+	const [urlValue, setURLValue] = useState('')
+	const [requestStatus, setRequestStatus] = useState('idle')
+	const [isLoading, setIsLoading] = useState(false)
+	const [stateFile, setStateFile] = useState(null)
 
-  const fetchURL = async () => {
-    setIsLoading(true);
-    try {
-      if (urlValue == '') {
-        setRequestStatus("error");
-      } else {
-        const result = await fetch(urlValue);
-        setStateFile(result);
-        setRequestStatus("success");
-      }
-    } catch (error) {
-      setRequestStatus("error");
-    } finally {
-      setIsLoading(false);
-    }
-  };
+	const fetchURL = async () => {
+		setIsLoading(true)
+		try {
+			if (urlValue == '') {
+				setRequestStatus('error')
+			} else {
+				const result = await fetch(urlValue)
+				setStateFile(result)
+				setRequestStatus('success')
+			}
+		} catch (error) {
+			setRequestStatus('error')
+		} finally {
+			setIsLoading(false)
+		}
+	}
 
-  const downloadHandler = async (stateFile) => {
-    const blob = await stateFile.blob();
-    const tempUrl = window.URL.createObjectURL(blob);
-    const aTag = document.createElement("a");
-    aTag.href = tempUrl;
-    aTag.download = tempUrl.replace(/^.*[\\\/]/, "");
-    aTag.click();
-    aTag.remove();
-    setRequestStatus("idle");
-    setURLValue("");
-  };
+	const downloadHandler = async stateFile => {
+		const blob = await stateFile.blob()
+		const tempUrl = window.URL.createObjectURL(blob)
+		const aTag = document.createElement('a')
+		aTag.href = tempUrl
+		aTag.download = tempUrl.replace('https://', '')
+		aTag.click()
+		aTag.remove()
+		setRequestStatus('idle')
+		setURLValue('')
+	}
 
-  const cancelerHandler = () => {
-    setURLValue("");
-    setRequestStatus("idle");
-  };
+	const cancelerHandler = () => {
+		setURLValue('')
+		setRequestStatus('idle')
+	}
 
-  return (
-    <>
-      <div className={styles["download-form__wrapper"]}>
-        <h1 className={styles["download-form__header"]}>Downloader</h1>
-        <input
-          className={styles["download-form__input"]}
-          name="url"
-          type="text"
-          value={urlValue}
-          onChange={(e) => {
-            setURLValue(e.target.value);
-          }}
-        />
-        <button
-          className={styles["download-form__button"]}
-          onClick={() => fetchURL(urlValue)}
-          disabled={isLoading}
-        >
-          Search
-        </button>
-        {isLoading && <div>Loading...</div>}
-        {requestStatus == "error" && (
-          <div style={{ color: "red" }}>Required file not found or link is not avalible</div>
-        )}
-        {requestStatus == "success" && (
-          <div>
-            <div>File for downloading is finded</div>
-            <div className={styles["download-form__button-wrapper"]}>
-              <button
-                className={styles["download-form__button-approve"]}
-                onClick={() => downloadHandler(stateFile)}
-              >
-                download
-              </button>
-              <button
-                className={styles["download-form__button-reject"]}
-                onClick={() => cancelerHandler()}
-              >
-                cancel
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={styles['download-form__wrapper']}>
+				<h1 className={styles['download-form__header']}>Downloader</h1>
+				<input
+					className={styles['download-form__input']}
+					name='url'
+					type='text'
+					value={urlValue}
+					onChange={e => {
+						setURLValue(e.target.value)
+					}}
+				/>
+				<button
+					className={styles['download-form__button']}
+					onClick={() => fetchURL(urlValue)}
+					disabled={isLoading}
+				>
+					{isLoading ? 'Loading...' : 'Search'}
+				</button>
+				<div
+					className={
+						requestStatus == 'error'
+							? styles['download-form__error-active']
+							: styles['download-form__error-hide']
+					}
+				>
+					Required file not found or link is not avalible
+				</div>
+				{requestStatus === 'success' && (
+					<div>
+						<div>File for downloading is finded</div>
+						<div className={styles['download-form__button-wrapper']}>
+							<button
+								className={styles['download-form__button-approve']}
+								onClick={() => downloadHandler(stateFile)}
+							>
+								download
+							</button>
+							<button
+								className={styles['download-form__button-reject']}
+								onClick={() => cancelerHandler()}
+							>
+								cancel
+							</button>
+						</div>
+					</div>
+				)}
+			</div>
+		</>
+	)
 }
